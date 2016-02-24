b9dbeedb00d77b2ba0276612ca9fdba4131d7916
Albert Chan <atfchan@gmail.com>
2016-02-25 01:19:17 +0800

    Added up migration script

--- app/migrations/0001_basic-schema.up.sql
+++ app/migrations/0001_basic-schema.up.sql
@@ -1,7 +1,9 @@
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     email TEXT NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    date_created TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    date_modified TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
 CREATE UNIQUE INDEX idx_users_email on users (email);
